90720beed5fbda9af1fc28a9ba9d6ed3a6150a79
Gabe McNunn <gmcnunn@sustainableenviro.com>
2019-07-26 10:41:06 -0500

    add output file xml spec

--- apsim/wrapper.py
+++ apsim/wrapper.py
@@ -43,16 +43,84 @@ def Init_Apsim_File():
 
     return apsim_xml
 
+
+###
+def Output_Variables( filename, var_list, freq ):
+    output_xml = Element( 'outputfile' )
+    filename = SubElement( output_xml, 'filename' )
+    filename.set( 'name', 'filename' )
+    filename.set( 'output', 'yes' )
+    filename.text = filename
+    title = SubElement( output_xml, 'title' )
+    title.text = filename.split( '.' )[0]
+    vars = SubElement( output_xml, 'variables' )
+    vars.set( 'name', 'Output Variables' )
+
+    for var in var_list:
+        var_elem = SubElement( vars, 'variable' )
+        var_elem.text = var
+
+    consts = SubElement( vars, 'constants' )
+    const = SubElement( consts, 'constant' )
+    const.set( 'name', 'precision' )
+    const.text = 5
+
+    evnts = SubElement( output_xml, 'events' )
+    evnts.set( 'name', 'Output variable events' )
+    evnt = SubElement( evnts, 'event' )
+    evnt.text = freq
+
+    return output_xml
+
+
+###
+def Init_SurfaceOM( crop, type, mass, cn_ratio, stand_frac ):
+    surfom_xml = Element( 'surfaceom' )
+    surfom_xml.set( 'name', 'SurfaceOrganicMatter' )
+    pool_elem = SubElement( surfom_xml, 'PoolName' )
+    pool_elem.set( 'description', 'Organic Matter pool name' )
+    pool_elem.set( 'type', 'text' )
+    pool_elem.text = crop
+    type_elem = SubElement( surfom_xml, 'type' )
+    type_elem.set( 'description', 'Organic Matter type' )
+    type_elem.set( 'type', 'text' )
+    type_elem.text = crop
+    mass_elem = SubElement( surfom_xml, 'mass' )
+    mass_elem.set( 'description', 'Initial surface residue (kg/ha)' )
+    mass_elem.set( 'type', 'text' )
+    mass_elem.text = mass
+    cnr_elem = SubElement( surfom_xml, 'cnr' )
+    cnr_elem.set( 'description', 'C:N ratio of initial residue' )
+    cnr_elem.set( 'type', 'text' )
+    cnr_elem.text = cn_ratio
+    sf_elem = SubElement( surfom_xml, 'standing_fraction' )
+    sf_elem.set( 'description', 'Fraction of residue standing' )
+    sf_elem.set( 'type', 'text' )
+    sf_elem.text = stand_frac
+
+    return surfom_xml
+
+
+
 #####
 mukeys = [ '403351' ]
 soils_lyr_df = Get_SSURGO_Soils_Data( mukeys, ssurgo_db )
 for mukey in mukeys:
+    apsim_xml = Init_Apsim_File()
+
+    outvars = [ 'dd/mm/yyyy as Date', 'day', 'year', 'yield', 'biomass',
+        'fertiliser', 'surfaceom_c', 'n2o_atm()', 'leach_no3' ]
+    output_xml = Output_Variables( mukey + '.out', outvars, 'daily' )
+
+    surfom_xml = Init_SurfaceOM( 'maize', 'maize', 3500, 65, 0.0 )
+
+    fert_xml = SubElement( apsim_xml, 'fertiliser' )
+
+    crop_xml = SubElement( apsim_xml, 'maize' )
+
     soil_df = soils_lyr_df.loc[ soils_lyr_df[ 'mukey' ] == mukey ]
     soil_xml = Create_Soil_XML( soil_df )
 
-apsim_xml = Init_Apsim_File()
-
-
-tree = ElementTree()
-tree._setroot( apsim_xml )
-tree.write( 'tester.apsim' )
+    tree = ElementTree()
+    tree._setroot( apsim_xml )
+    tree.write( 'tester.apsim' )
