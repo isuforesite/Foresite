ba992c5c0d274a075a60e3ecf5d34fe5852d9bde
Gabe McNunn <gmcnunn@sustainableenviro.com>
2019-08-02 11:31:01 -0500

    add to xml initialization

--- apsim/wrapper.py
+++ apsim/wrapper.py
@@ -13,30 +13,31 @@ host = '129.186.185.33'
 ssurgo_db = ConnectToDB( port, host, user, database )
 
 #####
-def Init_Apsim_File():
+def Init_Apsim_File( folder_name, sim_name, met_name, met_file, start_date,
+    end_date ):
     apsim_xml = Element( 'folder' )
     apsim_xml.set( 'version', '36' )
     apsim_xml.set( 'creator', 'Apsim_Wrapper' )
     apsim_xml.set( 'name', 'S1' )
     simgrp = SubElement( apsim_xml, 'folder' )
-    simgrp.set( 'name', 'Tests' )
+    simgrp.set( 'name', folder_name )
     sim = SubElement( simgrp, 'simulation' )
-    sim.set( 'name', 'Test Sim' )
+    sim.set( 'name', sim_name )
     metfile = SubElement( sim, 'metfile' )
-    metfile.set( 'name', 'met' )
+    metfile.set( 'name', met_name )
     filename = SubElement( metfile, 'filename' )
     filename.set( 'name', 'filename' )
     filename.set( 'input', 'yes' )
-    filename.text = 'MSF_daymet_1980_2017.met'
+    filename.text = met_file
     clock = SubElement( sim, 'clock' )
     clock_start = SubElement( clock, 'start_date' )
     clock_start.set( 'type', 'date' )
     clock_start.set( 'description', 'Enter the start date of the simulation' )
-    clock_start.text = '01/01/2007'
+    clock_start.text = start_date
     clock_end = SubElement( clock, 'end_date' )
     clock_end.set( 'type', 'date' )
     clock_end.set( 'description', 'Enter the end date of the simulation' )
-    clock_end.text = '31/12/2017'
+    clock_end.text = end_date
     sumfile = SubElement( sim, 'summaryfile' )
     area = SubElement( sim, 'area' )
     area.set( 'name', 'paddock' )
@@ -45,14 +46,14 @@ def Init_Apsim_File():
 
 
 ###
-def Output_Variables( filename, var_list, freq ):
+def Output_Variables( out_file, var_list, freq ):
     output_xml = Element( 'outputfile' )
     filename = SubElement( output_xml, 'filename' )
     filename.set( 'name', 'filename' )
     filename.set( 'output', 'yes' )
-    filename.text = filename
+    filename.text = out_file
     title = SubElement( output_xml, 'title' )
-    title.text = filename.split( '.' )[0]
+    title.text = out_file.split( '.' )[0]
     vars = SubElement( output_xml, 'variables' )
     vars.set( 'name', 'Output Variables' )
 
@@ -106,20 +107,30 @@ def Init_SurfaceOM( crop, type, mass, cn_ratio, stand_frac ):
 mukeys = [ '403351' ]
 soils_lyr_df = Get_SSURGO_Soils_Data( mukeys, ssurgo_db )
 for mukey in mukeys:
+    ### initialize sim
     apsim_xml = Init_Apsim_File()
 
+    ### soil data
+    soil_df = soils_lyr_df.loc[ soils_lyr_df[ 'mukey' ] == mukey ]
+    soil_xml = Create_Soil_XML( soil_df )
+    apsim_xml.append( soil_xml )
+
+    ### surface om
+    surfom_xml = Init_SurfaceOM( 'maize', 'maize', 3500, 65, 0.0 )
+
+    ### fertilizer
+    fert_xml = SubElement( apsim_xml, 'fertiliser' )
+
+    ### crops
+    crop_xml = SubElement( apsim_xml, 'maize' )
+
+    ### output file
     outvars = [ 'dd/mm/yyyy as Date', 'day', 'year', 'yield', 'biomass',
         'fertiliser', 'surfaceom_c', 'n2o_atm()', 'leach_no3' ]
     output_xml = Output_Variables( mukey + '.out', outvars, 'daily' )
 
-    surfom_xml = Init_SurfaceOM( 'maize', 'maize', 3500, 65, 0.0 )
-
-    fert_xml = SubElement( apsim_xml, 'fertiliser' )
-
-    crop_xml = SubElement( apsim_xml, 'maize' )
-
-    soil_df = soils_lyr_df.loc[ soils_lyr_df[ 'mukey' ] == mukey ]
-    soil_xml = Create_Soil_XML( soil_df )
+    ### management data
+    
 
     tree = ElementTree()
     tree._setroot( apsim_xml )
