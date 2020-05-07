e7c4d9901e5a5ed6c3f3a334abe37e38dfc998c7
mnowatzke <mattnowatzke@gmail.com>
2020-05-07 12:01:51 -0500

    added function to create excel met files, added helper functions

--- apsim/daymet.py
+++ apsim/daymet.py
@@ -3,6 +3,7 @@
 import requests
 import pandas as pd
 import io
+from analyses.munging import get_county, get_centroid 
 
 DAYMET_URL = 'https://daymet.ornl.gov/single-pixel/api/data'
 
@@ -282,3 +283,99 @@ class Weather:
 
         self.data = wth_df.append( self.data, sort = False )
         self.data = self.data.round( 2 )
+
+def create_excel_met(lat, long, start_year, end_year, county_name):
+    wth_obj = Weather().from_daymet(lat, long, 1980, 2019)
+    wth_df = wth_obj.data
+    tav = round(wth_df[ 'maxt' ].mean(), 1)
+    amp = round(wth_df['maxt'].max(), 1)
+    #greene_df.to_excel('greene.xlsx', index=False)
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.alignment = Alignment(horizontal="left")
+    for r in dataframe_to_rows(wth_df, index=False, header=True):
+        ws.append(r)
+    for cells in ws.iter_rows():
+        for cell in cells:
+            cell.alignment = Alignment(horizontal="left")
+    ws.insert_rows(2)
+    ws['A2'] = '()'
+    ws['B2'] = '()'
+    ws['C2'] = '(MJ/m2)'
+    ws['D2'] = '(oC)'
+    ws['E2'] = '(oC)'
+    ws['F2'] = '(mm)'
+    ws['G2'] = '(mm)'
+    ws['H2'] = '(kPa)'
+    ws['I2'] = '(hours)'
+    ws.insert_rows(1)
+    ws['A1'] = '!Weather generated using C-CHANGE Foresite framework'
+    ws.insert_rows(1)
+    ws['A1'] = f'amp = {amp}'
+    ws.insert_rows(1)
+    ws['A1'] = f'tav = {tav}'
+    ws.insert_rows(1)
+    ws['A1'] = f'longitude = {long} (DECIMAL DEGREES)'
+    ws.insert_rows(1)
+    ws['A1'] = f'latitude = {lat} (DECIMAL DEGREES)'
+    ws.insert_rows(1)
+    ws['A1'] = 'stationname = Daymet weather'
+    ws.insert_rows(1)
+    ws['A1'] = '[weather.met.weather]'
+    wb.save(f'apsim_files/{county_name}/met_files/{county_name}.xlsx')
+
+
+'''
+Get the weather for given centroid and write to a .met file
+
+Args:
+    lat {float} -- latitude of centroid
+    long {float} -- longitude of centroid
+    year_star {int} -- starting year of weather data
+    year_end {int} -- ending year of weather data
+    path {str} -- path to write the met files
+    filename {str} -- name to give the .met file
+
+Returns:
+    None
+'''
+def create_met(lat, long, start_year, end_year, filename, path='apsim_files/met_files'):
+    weather_obj = Weather().from_daymet(lat, long, start_year, end_year)
+    weather_obj.write_met_file(f'{path}/{filename}.met')
+
+
+    """
+Create all met files for each county in a given geometry.
+
+Args:
+    dbconn {database connection} -- connection to postgresql database
+    counties {array/list} -- every county to be run on in the geometry
+    table {str} -- name of the table within the database to get geometries from
+    id_column {str} -- table column that has unique ids (in this case fips) for each county.
+    geo_col {str} -- table column that has geometry.
+    name_col {str} -- column that has the name for each county - can just use fips columns if no county names in table.
+
+Returns:
+    Met file for each county in counties.
+"""
+def create_all_met(dbconn, counties, table, id_col='fips', geo_col='wkb_geometry', name_col='county'):
+    for i in counties:
+        county = get_county(dbconn, table, i, geo_col)
+        county_name = county[name_col][0].replace(" ", "_")
+        print(f'Geopandas table for {county_name} county created.')
+        centroid = get_centroid(county, id_col, geo_col)
+        print(f'Centroid located at {centroid}.')
+        weather = create_excel_met(centroid[0], centroid[1], 1980, 2019, county_name)
+        print(f"Met file for {county_name}/{i} at location {centroid} created.")
+
+def create_all_excel_met(dbconn, counties, table, id_col='fips', geo_col='wkb_geometry', name_col='county'):
+    for i in counties:
+        county = get_county(dbconn, table, i, geo_col)
+        county_name = county[name_col][0].replace(" ", "_")
+        print(f'Geopandas table for {county_name} county created.')
+        centroid = get_centroid(county, id_col, geo_col)
+        print(f'Centroid located at {centroid}.')
+        if not os.path.exists(f'apsim_files/{county_name}/met_files'):
+            os.makedirs(f'apsim_files/{county_name}/met_files')
+        create_excel_met(centroid[0], centroid[1], 1980, 2019, county_name)
+        print(f"Met file for {county_name}/{i} at location {centroid} created.")
