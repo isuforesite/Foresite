d98ab9691acea81f1fb2e9a9cc6f93decbda11a1
unknown <mattnowatzke@gmail.com>
2020-07-16 12:32:41 -0500

    moved calc_subfield_profit function

--- budgets/budget_calcs.py
+++ budgets/budget_calcs.py
@@ -199,38 +199,314 @@ class Budget:
         self.profit = self.revenue - self.expense_total
         return self.profit
 
-# #%%
-# columns = [
-#     'management_sample_id',
-#     'fertilizer',
-#     'seed',
-#     'preharv_machinery',
-#     'harvest_machinery',
-#     'processing',
-#     'chemicals',
-#     'extra',
-#     'labor',
-#     'revenue',
-#     'expenses',
-#     'profit'
-#     ]
-# output_df = pd.DataFrame(columns=columns)
-
-
-# def main():
-#     maize=Budget("cfs", "maize", 200, 100)
-#     maize.fert_cost(180, 40, 30, .38, .41, .31)
-#     maize.seed_cost(30000, 3.19)
-#     maize.preharvest_machinery_cost(3.9, 3.6, 4.6, 3.4, 4.3, 4.4, 2.7, 2.6, 4.8, 5.9, 2.0, 2.2)
-#     maize.harvest_machinery_cost(13.10, 6.7, 6.3, 3.0)
-#     maize.processing_cost(0.044, 0.038, 0.05, 0.17, 0.02, 0.02)
-#     maize.chemicals_cost(48.36, 15.16)
-#     maize.extra_costs(9.5, 219, rent=True)
-#     maize.labor_cost(2, 14.25)
-    #print(maize.calc_revenue(3.71))
-    #print(maize.sum_expenses())
-    #print(maize.calc_profit())
-    #print(maize.expenses)
+def calc_subfield_profit(clukey_list, county_df, year, cfs_mgmt, sfc_mgmt, cc_mgmt, rent=False, labor=0, labor_payrate=0):
+    df_rows = []
+    total_clukeys = len(clukey_list)
+    mukey_counter = 0
+    clukey_counter = 0
+    #get commodity prices
+    comm_price_df = pd.read_sql(f"SELECT * from public.hist_comm_prices WHERE year = '{year}';", dbconn)
+    corn_price = float(comm_price_df.loc[comm_price_df['commodity'] == 'corn', 'mrkt_avg'].iloc[0])
+    soy_price = float(comm_price_df.loc[comm_price_df['commodity'] == 'soybean', 'mrkt_avg'].iloc[0])
+    #get input costs
+    costs = pd.read_sql(f"SELECT * FROM public.crop_prod_costs WHERE year = '{year}'", dbconn)
+    corn_seed_price = float(costs.loc[costs['input'] == 'corn_seed', 'fixed-cost'].iloc[0])
+    soy_seed_price = float(costs.loc[costs['input'] == 'soy_seed', 'fixed-cost'].iloc[0])
+    n_price = float(costs.loc[costs['input'] == 'nitrogen', 'fixed-cost'].iloc[0])
+    phos_price = float(costs.loc[costs['input'] == 'phosphorous', 'fixed-cost'].iloc[0])
+    potash_price = float(costs.loc[costs['input'] == 'phosphorous', 'fixed-cost'].iloc[0])
+    fix_chisel = float(costs.loc[costs['input'] == 'chisel_plow', 'fixed-cost'].iloc[0])
+    var_chisel = float(costs.loc[costs['input'] == 'chisel_plow', 'variable-cost'].iloc[0])
+    fix_disk = float(costs.loc[costs['input'] == 'disk_field_cultivator', 'fixed-cost'].iloc[0])
+    var_disk = float(costs.loc[costs['input'] == 'disk_field_cultivator', 'variable-cost'].iloc[0])
+    fix_applicator = float(costs.loc[costs['input'] == 'nh3_applicator', 'fixed-cost'].iloc[0])
+    var_applicator = float(costs.loc[costs['input'] == 'nh3_applicator', 'variable-cost'].iloc[0])
+    fix_cultivator = float(costs.loc[costs['input'] == 'field_cultivator', 'fixed-cost'].iloc[0])
+    var_cultivator = float(costs.loc[costs['input'] == 'field_cultivator', 'variable-cost'].iloc[0])
+    fix_planter = float(costs.loc[costs['input'] == 'planter', 'fixed-cost'].iloc[0])
+    var_planter = float(costs.loc[costs['input'] == 'planter', 'variable-cost'].iloc[0])
+    fix_sprayer = float(costs.loc[costs['input'] == 'sprayer', 'fixed-cost'].iloc[0])
+    var_sprayer = float(costs.loc[costs['input'] == 'sprayer', 'variable-cost'].iloc[0])
+    fix_corn_combine = float(costs.loc[costs['input'] == 'combine_corn', 'fixed-cost'].iloc[0])
+    var_corn_combine = float(costs.loc[costs['input'] == 'combine_corn', 'variable-cost'].iloc[0])
+    fix_soy_combine = float(costs.loc[costs['input'] == 'combine_soybeans', 'fixed-cost'].iloc[0])
+    var_soy_combine = float(costs.loc[costs['input'] == 'combine_soybeans', 'variable-cost'].iloc[0])
+    fix_wagon = float(costs.loc[costs['input'] == 'grain_cart', 'fixed-cost'].iloc[0])
+    var_wagon = float(costs.loc[costs['input'] == 'grain_cart', 'variable-cost'].iloc[0])
+    fix_hauling = float(costs.loc[costs['input'] == 'haul_grain', 'fixed-cost'].iloc[0])
+    var_hauling = float(costs.loc[costs['input'] == 'haul_grain', 'variable-cost'].iloc[0])
+    fix_drying = float(costs.loc[costs['input'] == 'dry_grain', 'fixed-cost'].iloc[0])
+    var_drying = float(costs.loc[costs['input'] == 'dry_grain', 'variable-cost'].iloc[0])
+    fix_handle = float(costs.loc[costs['input'] == 'auger_store_grain', 'fixed-cost'].iloc[0])
+    var_handle = float(costs.loc[costs['input'] == 'auger_store_grain', 'variable-cost'].iloc[0])
+    soy_herb_cost = float(costs.loc[(costs['crop'] == 'soy') & (costs['input'] == 'herbicide'), 'fixed-cost'].iloc[0])
+    corn_herb_cost = float(costs.loc[(costs['crop'] == 'corn') & (costs['input'] == 'herbicide'), 'fixed-cost'].iloc[0])
+    insect_cost = float(costs.loc[costs['input'] == 'insecticide', 'fixed-cost'].iloc[0])
+    corn_crop_insurance = float(costs.loc[(costs['crop'] == 'corn') & (costs['input'] == 'crop_insurance'), 'variable-cost'].iloc[0])
+    soy_crop_insurance = float(costs.loc[(costs['crop'] == 'soy') & (costs['input'] == 'crop_insurance'), 'variable-cost'].iloc[0])
+    rent_cost = float(costs.loc[costs['input'] == 'rent', 'fixed-cost'].iloc[0])
+    misc_cost = float(costs.loc[costs['input'] == 'miscellaneous', 'variable-cost'].iloc[0])
+    for i in clukey_list:
+        try:
+            print(f'Getting data for clukey {i}')
+            clukey_counter += 1
+            rot_df = pd.read_sql(f"SELECT * FROM raccoon.raccoon_rots WHERE clukey = {i};", dbconn)
+            clukey_df = county_df.loc[county_df['clukey'] == i].reset_index(drop=True)
+            rotation = get_rotation(rot_df, 'crop')
+            for i in clukey_df.index:
+                try:
+                    state = clukey_df.loc[i, 'state']
+                    fips = clukey_df.loc[i, 'fips']
+                    huc8 = clukey_df.loc[i, 'huc8']
+                    county = clukey_df.loc[i, 'county']
+                    geom = clukey_df.loc[i, 'wkb_geometry']
+                    mukey = clukey_df.loc[i, 'mukey']
+                    musym = clukey_df.loc[i, 'musym']
+                    try:
+                        mukey_summary = pd.read_sql(f"SELECT * FROM raccoon.apsim_output_summary WHERE county = '{county}' AND mukey = {mukey} AND rotation = '{rotation}';", dbconn)
+                        if mukey_summary.empty:
+                            continue
+                    except Exception:
+                        traceback.print_exc()
+                        continue
+                    acreage = float(clukey_df.loc[i, 'acres'])
+                    no3_loss = float((mukey_summary.iloc[0]['leach_no3']/2.471)*acreage)
+                    clukey = clukey_df.loc[i, 'clukey']
+                    if rotation == 'cc':
+                        current_crop = 'corn'
+                        buac = float(mukey_summary.iloc[0]['corn_buac'])
+                        n_lbs = float(mukey_summary.iloc[0]['fertiliser'])*0.892
+                        #calculate how much phos and potash to add based on yield
+                        phos_lbs = buac*0.37
+                        potash_lbs = buac*0.37
+                        #plant density is in m2 in apsim -- 10000 sq meters in hectare, 2.47 acres in hectare -- seeding rate = plant density * 10000 / 2.47
+                        sowing_density = (int(cc_mgmt['sowing_density'])*10000)/2.47
+                        cc_budget = Budget(rotation, current_crop, buac, acreage, year)
+                        fert_cost = cc_budget.fert_cost(n_lbs, n_price, phos_lbs, phos_price, potash_lbs, potash_price)
+                        seed_cost = cc_budget.seed_cost(sowing_density, corn_seed_price)
+                        preharvest_machinery_cost = cc_budget.preharvest_machinery_cost(
+                            fix_chisel=fix_chisel,
+                            var_chisel=var_chisel,
+                            fix_disk=fix_disk,
+                            var_disk=var_disk,
+                            fix_applicator=fix_applicator,
+                            var_applicator=var_applicator,
+                            fix_planter=fix_planter,
+                            var_planter=var_planter,
+                            fix_sprayer=fix_sprayer,
+                            var_sprayer=var_sprayer)
+                        harvest_machinery_cost = cc_budget.harvest_machinery_cost(fix_corn_combine, var_corn_combine, fix_wagon, var_wagon)
+                        processing_cost = cc_budget.processing_cost(fix_hauling, var_hauling, fix_drying, var_drying, fix_handle, var_handle)
+                        chemicals_cost = cc_budget.chemicals_cost(corn_herb_cost, insect_cost)
+                        extra_costs = cc_budget.extra_costs(misc_cost, corn_crop_insurance)
+                        labor_cost = cc_budget.labor_cost()
+                        revenue = cc_budget.calc_revenue(corn_price)
+                        expenses = cc_budget.sum_expenses()
+                        profit = cc_budget.calc_profit()
+                        df_rows.append((
+                            state,
+                            huc8,
+                            fips,
+                            county,
+                            clukey,
+                            mukey,
+                            musym,
+                            acreage,
+                            rotation,
+                            current_crop,
+                            buac,
+                            no3_loss,
+                            n_lbs,
+                            phos_lbs,
+                            potash_lbs,
+                            sowing_density,
+                            fert_cost,
+                            seed_cost,
+                            preharvest_machinery_cost,
+                            harvest_machinery_cost,
+                            processing_cost,
+                            chemicals_cost,
+                            extra_costs,
+                            labor_cost,
+                            revenue,
+                            expenses,
+                            profit,
+                            geom)
+                        )
+                    elif rotation == 'cfs':
+                        current_crop = 'corn'
+                        buac = float(mukey_summary.iloc[0]['corn_buac'])
+                        n_lbs = float(mukey_summary.iloc[0]['fertiliser'])*0.892
+                        #calculate how much phos and potash to add based on yield
+                        phos_lbs = buac*0.375
+                        potash_lbs = buac*0.3
+                        sowing_density = (int(cfs_mgmt['sowing_density'])*10000)/2.47
+                        cfs_budget = Budget(rotation, current_crop, buac, acreage, year)
+                        fert_cost = cfs_budget.fert_cost(n_lbs, n_price, phos_lbs, phos_price, potash_lbs, potash_price)
+                        seed_cost = cfs_budget.seed_cost(sowing_density, corn_seed_price)
+                        preharvest_machinery_cost = cfs_budget.preharvest_machinery_cost(
+                            fix_chisel=fix_chisel,
+                            var_chisel=var_chisel,
+                            fix_disk=fix_disk,
+                            var_disk=var_disk,
+                            fix_applicator=fix_applicator,
+                            var_applicator=var_applicator,
+                            fix_planter=fix_planter,
+                            var_planter=var_planter,
+                            fix_sprayer=fix_sprayer,
+                            var_sprayer=var_sprayer)
+                        harvest_machinery_cost = cfs_budget.harvest_machinery_cost(fix_corn_combine, var_corn_combine, fix_wagon, var_wagon)
+                        processing_cost = cfs_budget.processing_cost(fix_hauling, var_hauling, fix_drying, var_drying, fix_handle, var_handle)
+                        chemicals_cost = cfs_budget.chemicals_cost(corn_herb_cost)
+                        extra_costs = cfs_budget.extra_costs(misc_cost, corn_crop_insurance)
+                        labor_cost = cfs_budget.labor_cost()
+                        revenue = cfs_budget.calc_revenue(corn_price)
+                        expenses = cfs_budget.sum_expenses()
+                        profit = cfs_budget.calc_profit()
+                        df_rows.append((
+                            state,
+                            huc8,
+                            fips,
+                            county,
+                            clukey,
+                            mukey,
+                            musym,
+                            acreage,
+                            rotation,
+                            current_crop,
+                            buac,
+                            no3_loss,
+                            n_lbs,
+                            phos_lbs,
+                            potash_lbs,
+                            sowing_density,
+                            fert_cost,
+                            seed_cost,
+                            preharvest_machinery_cost,
+                            harvest_machinery_cost,
+                            processing_cost,
+                            chemicals_cost,
+                            extra_costs,
+                            labor_cost,
+                            revenue,
+                            expenses,
+                            profit,
+                            geom)
+                        )
+                    elif rotation == 'sfc':
+                        current_crop = 'soybean'
+                        buac = float(mukey_summary.iloc[0]['soy_buac'])
+                        #calculate how much phos and potash to add based on yield
+                        phos_lbs = buac*0.8
+                        potash_lbs = buac*1.51
+                        n_lbs = 0
+                        sowing_density = (int(sfc_mgmt['sowing_density'])*10000)/2.47
+                        spray_fix = fix_sprayer*2
+                        spray_var = var_sprayer*2
+                        sfc_budget = Budget(rotation, current_crop, buac, acreage, year)
+                        fert_cost = sfc_budget.fert_cost(n_lbs, n_price, phos_lbs, phos_price, potash_lbs, potash_price)
+                        seed_cost = sfc_budget.seed_cost(sowing_density, soy_seed_price)
+                        preharvest_machinery_cost = sfc_budget.preharvest_machinery_cost(
+                            fix_chisel=fix_chisel,
+                            var_chisel=var_chisel,
+                            fix_disk=fix_disk,
+                            var_disk=var_disk,
+                            fix_applicator=fix_applicator,
+                            var_applicator=var_applicator,
+                            fix_planter=fix_planter,
+                            var_planter=var_planter,
+                            fix_sprayer=spray_fix,
+                            var_sprayer=spray_var)
+                        harvest_machinery_cost = sfc_budget.harvest_machinery_cost(fix_soy_combine, var_soy_combine, fix_wagon, var_wagon)
+                        processing_cost = sfc_budget.processing_cost(fix_hauling, var_hauling, fix_drying, var_drying, fix_handle, var_handle)
+                        chemicals_cost = sfc_budget.chemicals_cost(soy_herb_cost)
+                        extra_costs = sfc_budget.extra_costs(misc_cost, soy_crop_insurance)
+                        labor_cost = sfc_budget.labor_cost()
+                        revenue = sfc_budget.calc_revenue(soy_price)
+                        expenses = sfc_budget.sum_expenses()
+                        profit = sfc_budget.calc_profit()
+                        df_rows.append((
+                            state,
+                            huc8,
+                            fips,
+                            county,
+                            clukey,
+                            mukey,
+                            musym,
+                            acreage,
+                            rotation,
+                            current_crop,
+                            buac,
+                            no3_loss,
+                            n_lbs,
+                            phos_lbs,
+                            potash_lbs,
+                            sowing_density,
+                            fert_cost,
+                            seed_cost,
+                            preharvest_machinery_cost,
+                            harvest_machinery_cost,
+                            processing_cost,
+                            chemicals_cost,
+                            extra_costs,
+                            labor_cost,
+                            revenue,
+                            expenses,
+                            profit,
+                            geom)
+                        )
+                    else:
+                        print('Not a corn or soybean rotation.')
+                        continue
+                    mukey_counter += 1
+                    if mukey_counter % 50 == 0:
+                        print(f"Finished with {mukey_counter} mukeys")
+                    if clukey_counter == total_clukeys:
+                        print("All done")
+                except Exception:
+                    traceback.print_exc()
+                    print(f'failed on mukey {mukey}')
+                    mukey_counter +=1
+                    if mukey_counter % 50 == 0:
+                        print(f"Finished with {mukey_counter} mukeys")
+                    if clukey_counter == total_clukeys:
+                        print("All done")
+                    continue
+        except Exception:
+            traceback.print_exc()
+            continue
+    return gpd.GeoDataFrame(df_rows,
+    #return pd.DataFrame(df_rows,
+                        columns=(
+                            "state",
+                            "huc8",
+                            "fips",
+                            "county",
+                            "clukey",
+                            "mukey",
+                            "musym",
+                            "acreage",
+                            "rotation",
+                            "crop",
+                            "buac",
+                            "no3_loss",
+                            "n_lbs",
+                            "phos_lbs",
+                            "potash_lbs",
+                            "sowing_density",
+                            "fert_cost",
+                            "seed_cost",
+                            "preharv_machinery",
+                            "harv_machinery",
+                            "processing",
+                            "chemicals",
+                            "extra_costs",
+                            "labor_cost",
+                            "revenue",
+                            "expenses",
+                            "profit",
+                            "wkb_geometry"
+                        ), geometry="wkb_geometry")
     
 #%%
 if __name__ == "__main__":
