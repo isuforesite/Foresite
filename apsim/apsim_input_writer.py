ed0308e8ada332f7b0494c9d8421b9de8e5f9273
Matt Nowatzke <mattnowatzke@gmail.com>
2021-01-06 11:26:56 -0600

    fixed fertilizer bug with updated keys

--- apsim/apsim_input_writer.py
+++ apsim/apsim_input_writer.py
@@ -42,6 +42,7 @@ def get_rot_year_one(years):
 def get_rot_year_two(years):
     return years[1::2]
 
+###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!###
 #Set all of the json keys to be parsed over
 #tillage keys
 tillage_implement_key = 'tillage_implement'
@@ -49,9 +50,9 @@ tillage_depth_key = 'tillage_depth'
 tillage_f_incorp_key = 'tillage_residue_incorporation'
 tillage_date_key = 'tillage_timing'
 #fertilizer keys
-fert_amount_key = 'fert_kg_n_ha'
-fert_date_key = 'fert_fertilize_n_on'
-fert_formula_key = 'fert_n_fertilizer'
+fert_amount_key = 'kg_n_ha'
+fert_date_key = 'fertilize_n_on'
+fert_formula_key = 'n_fertilizer'
 fert_depth_key = 'fert_depth'
 #planting keys
 plant_crop_key = 'sow_crop'
@@ -65,163 +66,163 @@ harvest_crop_key = 'harvest'
 harvest_date_key = 'harvest_date'
 
 #def create_input_table(dbconn, table, fip, start_year=2016, end_year=2018, id_col='fips', geo_col='wkb_geometry', name_col='county', soil_col="mukey", limit=False):
-def create_apsim_files(df, rotations_df, dbconn, field_key='clukey', soil_key='mukey', county_col='county', rotation_col='rotation', crop_col='crop', start_year=2016, end_year=2018):
-    if not os.path.exists('apsim_files'):
-        os.makedirs('apsim_files')
-    start_date = f'01/01/{start_year}'
-    end_date = f'31/12/{end_year}'
-    #save rotation for clukey to crops list
-    #loop through field keys e.g., clukeys
-    sim_count = 0
-    for i in df[field_key]:
-        field_id = i
-        #get field information
-        #TODO get 'clukey' and 'county' to work as function inputs instead of hardcoded
-        field = df.loc[df['clukey'] == i]
-        #get field rotation
-        #rotation_row = rotations_df.loc[rotations_df[field_key] == i]
-        rotation = get_rotation(rotations_df, crop_col)
-        #get unique soil keys e.g., mukeys
-        soils = field.drop_duplicates(soil_key)
-        runs = soils['mukey']
-        #get weather file for desired county
-        county_name = field.iloc[0]['county'].replace(" ", "_")
-        met_name = f"{county_name}.met"
-        met_path = f"met_files/{met_name}"
-        #create apsim file for each unique soil in field
-        for i in runs:
-            try:
-                soil_id = i
-                soil_query = '''select * from api.get_soil_properties( array[{}]::text[] )'''.format( i )
-                soil_df = pd.read_sql( soil_query, dbconn )
-                if soil_df.empty:
-                    continue
-                #soil_row = soils_df.loc[soils_df[f'{soil_key}'] == i]
-                #initialize .apsim xml
-                apsim_xml = Element( 'folder' )
-                apsim_xml.set( 'version', '36' )
-                apsim_xml.set( 'creator', 'C-CHANGE Foresite' )
-                apsim_xml.set( 'name', county_name )
-                sim = SubElement( apsim_xml, 'simulation' )
-                sim.set( 'name', f'{county_name} {field_id}' )
+# def create_apsim_files(df, rotations_df, dbconn, field_key='clukey', soil_key='mukey', county_col='county', rotation_col='rotation', crop_col='crop', start_year=2016, end_year=2018):
+#     if not os.path.exists('apsim_files'):
+#         os.makedirs('apsim_files')
+#     start_date = f'01/01/{start_year}'
+#     end_date = f'31/12/{end_year}'
+#     #save rotation for clukey to crops list
+#     #loop through field keys e.g., clukeys
+#     sim_count = 0
+#     for i in df[field_key]:
+#         field_id = i
+#         #get field information
+#         #TODO get 'clukey' and 'county' to work as function inputs instead of hardcoded
+#         field = df.loc[df['clukey'] == i]
+#         #get field rotation
+#         #rotation_row = rotations_df.loc[rotations_df[field_key] == i]
+#         rotation = get_rotation(rotations_df, crop_col)
+#         #get unique soil keys e.g., mukeys
+#         soils = field.drop_duplicates(soil_key)
+#         runs = soils['mukey']
+#         #get weather file for desired county
+#         county_name = field.iloc[0]['county'].replace(" ", "_")
+#         met_name = f"{county_name}.met"
+#         met_path = f"met_files/{met_name}"
+#         #create apsim file for each unique soil in field
+#         for i in runs:
+#             try:
+#                 soil_id = i
+#                 soil_query = '''select * from api.get_soil_properties( array[{}]::text[] )'''.format( i )
+#                 soil_df = pd.read_sql( soil_query, dbconn )
+#                 if soil_df.empty:
+#                     continue
+#                 #soil_row = soils_df.loc[soils_df[f'{soil_key}'] == i]
+#                 #initialize .apsim xml
+#                 apsim_xml = Element( 'folder' )
+#                 apsim_xml.set( 'version', '36' )
+#                 apsim_xml.set( 'creator', 'C-CHANGE Foresite' )
+#                 apsim_xml.set( 'name', county_name )
+#                 sim = SubElement( apsim_xml, 'simulation' )
+#                 sim.set( 'name', f'{county_name} {field_id}' )
                 
-                #set met file
-                metfile = SubElement( sim, 'metfile' )
-                metfile.set( 'name', f'{county_name}' )
-                filename = SubElement( metfile, 'filename' )
-                filename.set( 'name', 'filename' )
-                filename.set( 'input', 'yes' )
-                filename.text = met_path
+#                 #set met file
+#                 metfile = SubElement( sim, 'metfile' )
+#                 metfile.set( 'name', f'{county_name}' )
+#                 filename = SubElement( metfile, 'filename' )
+#                 filename.set( 'name', 'filename' )
+#                 filename.set( 'input', 'yes' )
+#                 filename.text = met_path
 
-                #set clock
-                clock = SubElement( sim, 'clock' )
-                clock_start = SubElement( clock, 'start_date' )
-                clock_start.set( 'type', 'date' )
-                clock_start.set( 'description', 'Enter the start date of the simulation' )
-                clock_start.text = start_date
-                clock_end = SubElement( clock, 'end_date' )
-                clock_end.set( 'type', 'date' )
-                clock_end.set( 'description', 'Enter the end date of the simulation' )
-                clock_end.text = end_date
-                sumfile = SubElement( sim, 'summaryfile' )
-                area = SubElement( sim, 'area' )
-                area.set( 'name', 'paddock' )
+#                 #set clock
+#                 clock = SubElement( sim, 'clock' )
+#                 clock_start = SubElement( clock, 'start_date' )
+#                 clock_start.set( 'type', 'date' )
+#                 clock_start.set( 'description', 'Enter the start date of the simulation' )
+#                 clock_start.text = start_date
+#                 clock_end = SubElement( clock, 'end_date' )
+#                 clock_end.set( 'type', 'date' )
+#                 clock_end.set( 'description', 'Enter the end date of the simulation' )
+#                 clock_end.text = end_date
+#                 sumfile = SubElement( sim, 'summaryfile' )
+#                 area = SubElement( sim, 'area' )
+#                 area.set( 'name', 'paddock' )
 
-                # add soil xml
-                soil = apsim.Soil( soil_df, SWIM = False, SaxtonRawls = False )
-                area.append( soil.soil_xml() )
-                ### surface om
-                surfom_xml = apsim.init_surfaceOM( 'maize', 'maize', 3500, 65, 0.0 )
-                area.append( surfom_xml )
-                ### fertilizer
-                fert_xml = SubElement( area, 'fertiliser' )
+#                 # add soil xml
+#                 soil = apsim.Soil( soil_df, SWIM = False, SaxtonRawls = False )
+#                 area.append( soil.soil_xml() )
+#                 ### surface om
+#                 surfom_xml = apsim.init_surfaceOM( 'maize', 'maize', 3500, 65, 0.0 )
+#                 area.append( surfom_xml )
+#                 ### fertilizer
+#                 fert_xml = SubElement( area, 'fertiliser' )
 
-                ### crops
-                crop_xml = SubElement( area, 'maize' )
-                crop_xml = SubElement( area, 'soybean' )
-                crop_xml = SubElement( area, 'wheat' )
+#                 ### crops
+#                 crop_xml = SubElement( area, 'maize' )
+#                 crop_xml = SubElement( area, 'soybean' )
+#                 crop_xml = SubElement( area, 'wheat' )
 
-                ### output file
-                outvars = [
-                    'title',
-                    'dd/mm/yyyy as date',
-                    'day',
-                    'year',
-                    'soybean.yield as soybean_yield',
-                    'maize.yield as maize_yield',
-                    'soybean.biomass as soybean_biomass',
-                    'maize.biomass as maize_biomass',
-                    'corn_buac',
-                    'soy_buac',
-                    'fertiliser',
-                    'surfaceom_c',
-                    'subsurface_drain',
-                    'subsurface_drain_no3',
-                    'leach_no3'
-                ]
-                output_xml = apsim.set_output_variables( f'{county_name}_{field_id}_{soil_id}.out', outvars )
-                area.append( output_xml )
+#                 ### output file
+#                 outvars = [
+#                     'title',
+#                     'dd/mm/yyyy as date',
+#                     'day',
+#                     'year',
+#                     'soybean.yield as soybean_yield',
+#                     'maize.yield as maize_yield',
+#                     'soybean.biomass as soybean_biomass',
+#                     'maize.biomass as maize_biomass',
+#                     'corn_buac',
+#                     'soy_buac',
+#                     'fertiliser',
+#                     'surfaceom_c',
+#                     'subsurface_drain',
+#                     'subsurface_drain_no3',
+#                     'leach_no3'
+#                 ]
+#                 output_xml = apsim.set_output_variables( f'{county_name}_{field_id}_{soil_id}.out', outvars )
+#                 area.append( output_xml )
 
-                graph_no3 = [
-                    'Cumulative subsurface_drain',
-                    'Cumulative subsurface_drain_no3',
-                    'Cumulative leach_no3'
-                ]
-                graph_yield = [
-                    'soybean_yield',
-                    'maize_yield',
-                    'soybean_biomass',
-                    'maize_biomass',
-                    'soy_buac',
-                    'corn_buac'
-                ]
-                graph_all = [
-                    'soybean_yield',
-                    'maize_yield',
-                    'soybean_biomass',
-                    'maize_biomass',
-                    'corn_buac',
-                    'soy_buac',
-                    'fertiliser',
-                    'surfaceom_c',
-                    'subsurface_drain',
-                    'subsurface_drain_no3',
-                    'leach_no3' 
-                ]
+#                 graph_no3 = [
+#                     'Cumulative subsurface_drain',
+#                     'Cumulative subsurface_drain_no3',
+#                     'Cumulative leach_no3'
+#                 ]
+#                 graph_yield = [
+#                     'soybean_yield',
+#                     'maize_yield',
+#                     'soybean_biomass',
+#                     'maize_biomass',
+#                     'soy_buac',
+#                     'corn_buac'
+#                 ]
+#                 graph_all = [
+#                     'soybean_yield',
+#                     'maize_yield',
+#                     'soybean_biomass',
+#                     'maize_biomass',
+#                     'corn_buac',
+#                     'soy_buac',
+#                     'fertiliser',
+#                     'surfaceom_c',
+#                     'subsurface_drain',
+#                     'subsurface_drain_no3',
+#                     'leach_no3' 
+#                 ]
 
-                output_xml.append( apsim.add_xy_graph( 'Date', graph_no3, 'no3' ) )
-                output_xml.append( apsim.add_xy_graph( 'Date', graph_yield, 'yield' ) )
-                output_xml.append( apsim.add_xy_graph( 'Date', graph_all, 'all outputs' ) )
+#                 output_xml.append( apsim.add_xy_graph( 'Date', graph_no3, 'no3' ) )
+#                 output_xml.append( apsim.add_xy_graph( 'Date', graph_yield, 'yield' ) )
+#                 output_xml.append( apsim.add_xy_graph( 'Date', graph_all, 'all outputs' ) )
 
-                op_man = apsim.OpManager()
-                op_man.add_empty_manager()
-                if rotation == 'cfs':
-                    add_management_year(op_man, cfs_mgmt, 2016)
-                    add_management_year(op_man, sfc_mgmt, 2017)
-                    add_management_year(op_man, cfs_mgmt, 2018)
-                elif rotation == 'sfc':
-                    add_management_year(op_man, sfc_mgmt, 2016)
-                    add_management_year(op_man, cfs_mgmt, 2017)
-                    add_management_year(op_man, sfc_mgmt, 2018)
-                elif rotation == 'cc':
-                    add_management_year(op_man, cc_mgmt, 2016)
-                    add_management_year(op_man, cc_mgmt, 2017)
-                    add_management_year(op_man, cc_mgmt, 2018)
-                else:
-                    continue
-                area.append( op_man.man_xml )
-                outfile = f'apsim_files/{county_name}_{field_id}_{soil_id}.apsim'
-                ### management data
-                tree = ElementTree()
-                tree._setroot( apsim_xml )
-                tree.write( outfile )
-                sim_count += 1
-                if (sim_count % 5 == 0):
-                    print(f'Finished with {sim_count} files.')
-            except:
-                print(f'File creation failed for APSIM run {sim_count}')
-                sim_count +=1
-                continue
+#                 op_man = apsim.OpManager()
+#                 op_man.add_empty_manager()
+#                 if rotation == 'cfs':
+#                     add_management_year(op_man, cfs_mgmt, 2016)
+#                     add_management_year(op_man, sfc_mgmt, 2017)
+#                     add_management_year(op_man, cfs_mgmt, 2018)
+#                 elif rotation == 'sfc':
+#                     add_management_year(op_man, sfc_mgmt, 2016)
+#                     add_management_year(op_man, cfs_mgmt, 2017)
+#                     add_management_year(op_man, sfc_mgmt, 2018)
+#                 elif rotation == 'cc':
+#                     add_management_year(op_man, cc_mgmt, 2016)
+#                     add_management_year(op_man, cc_mgmt, 2017)
+#                     add_management_year(op_man, cc_mgmt, 2018)
+#                 else:
+#                     continue
+#                 area.append( op_man.man_xml )
+#                 outfile = f'apsim_files/{county_name}_{field_id}_{soil_id}.apsim'
+#                 ### management data
+#                 tree = ElementTree()
+#                 tree._setroot( apsim_xml )
+#                 tree.write( outfile )
+#                 sim_count += 1
+#                 if (sim_count % 5 == 0):
+#                     print(f'Finished with {sim_count} files.')
+#             except:
+#                 print(f'File creation failed for APSIM run {sim_count}')
+#                 sim_count +=1
+#                 continue
 
 def create_mukey_county_runs(soils_list, dbconn, rotation, met_name, county_name, fips, start_year=2016, end_year=2018, swim = False, saxton=True, sfc_mgmt=None, cfs_mgmt=None, cc_mgmt=None):
     runs_folder_path = f'apsim_files/{county_name}/{end_year}/{rotation}'
