488484a82caa3c31c6d1cecaa6b0501c09602055
Matt Nowatzke <mattnowatzke@gmail.com>
2021-02-19 11:35:13 -0600

    removed old files

--- tests/apsimtest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-#create test class that inherits from unittest class
-class TestApsimWriter(unittest.TestCase):
-    
-    def test_apsimwriter(self):
-        #eg. self.assertEqual()
-        pass
