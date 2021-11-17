8988a13962cf387b33baee686c73d8ff89624b89
Matt Nowatzke <mattnowatzke@gmail.com>
2021-11-17 12:30:39 -0600

    new functions for parsing all and summary apsim output

--- apsim/apsim_output_parser.py
+++ apsim/apsim_output_parser.py
@@ -187,7 +187,7 @@ def parse_summary_output_county(out_file_dir, year=2019): #, db_path, db_schema,
     #     index = False )
 
 def parse_all_output_field(out_file_dir, year=2019): #, db_path, db_schema, db_table
-    """Parses all .out files for a county, gets daily output and returns df for the given year.
+    """Parses all .out files for a given field, gets daily output and returns df for the given year.
     Arguments:
         out_file_dir {str} -- path to folder that contains .out files
         year (int) -- the targeted year of simulation data
@@ -360,6 +360,209 @@ def parse_summary_output_field(out_file_dir, year, swim=False): #, db_path, db_s
     #     schema = db_schema,
     #     if_exists = 'replace',
     #     index = False )
+
+
+def parse_summary_output(out_file, year=None, swim=False): #, db_path, db_schema, db_table
+    """Parses and summarizes output (.out) for a given year.
+    Arguments:
+        out_file {str} -- APSIM .out file to read. 
+        year (int) -- year of output to parse.
+    Returns:
+        [object] -- df with summary data for each .out file
+    """
+    push_data = []
+    # read file
+    daily_df = pd.read_csv( out_file, header = 3, delim_whitespace = True )
+    daily_df = daily_df[1:]
+    #get field name, mukey, and rotation with regex
+    df_header = daily_df['title'][1]
+    name_pattern = "name_(.*?)_mukey"
+    field = str(re.search(name_pattern, df_header).group(1))
+    mukey_pattern = "mukey_(.*?)_rot"
+    mukey = str(re.search(mukey_pattern, df_header).group(1))
+    rot_pattern = "rot_(.*?)_sim"
+    rotation = str(re.search(rot_pattern, df_header).group(1))
+    #insert new columns with field name, mukey, rot
+    daily_df.insert(1, 'field', field)
+    daily_df.insert(2, 'mukey', mukey)
+    daily_df.insert(3, 'rotation', rotation)
+    daily_df = daily_df.reset_index( drop = True )
+    #drop first row that has unit names
+    #replace all ? with NaN
+    daily_df = daily_df.replace("?", np.nan)
+    del daily_df['date']
+    
+    # cast explicit data types for processing
+    data_type_dict = {
+        'title' : 'string',
+        'field' : 'string',
+        'mukey' : 'string',
+        'rotation' : 'string',
+        'day': 'int64',
+        'year': 'int64',
+        'soybean_yield': 'float64',
+        'maize_yield': 'float64',
+        'soy_mktyd': 'float64',
+        'maz_mktyd': 'float64',
+        'soy_ymgha': 'float64',
+        'maz_ymgha': 'float64',
+        'soybean_biomass': 'float64',
+        'maize_biomass': 'float64',
+        'fertiliser': 'float64',
+        #'n2o_atm': 'float64',
+        #'surfaceom_c': 'float64',
+        'leach_no3': 'float64',
+        'Rain' : 'float64',
+        'drain' : 'float64',
+        'corn_buac' : 'float64',
+        'soy_buac' : 'float64'
+        }
+    if swim == True:
+        swim_dict = {
+        'subsurface_drain': 'float64',
+        'subsurface_drain_no3': 'float64'
+        }
+        data_type_dict.update(swim_dict)
+    
+    daily_df = daily_df.astype( data_type_dict )
+    if year != None:
+        df_year = daily_df.loc[daily_df['year'] == year].reset_index(drop=True)
+        data = {
+        'title' : df_header,
+        'field' : field,
+        'mukey' : mukey,
+        'rotation' : rotation,
+        'year': int(year),
+        'soybean_yield': df_year[ 'soybean_yield' ].max(),
+        'maize_yield': df_year[ 'maize_yield' ].max(),
+        'soy_mktyd': df_year[ 'soy_mktyd' ].max(),
+        'maz_mktyd': df_year[ 'maz_mktyd' ].max(),
+        'soy_ymgha': df_year[ 'soy_ymgha' ].max(),
+        'maz_ymgha': df_year[ 'maz_ymgha' ].max(),
+        'corn_buac' : df_year[ 'corn_buac' ].max(),
+        'soy_buac' : df_year[ 'soy_buac' ].max(),
+        'soybean_biomass': df_year[ 'soybean_biomass' ].max(),
+        'maize_biomass': df_year[ 'maize_biomass' ].max(),
+        'fertiliser': df_year[ 'fertiliser' ].sum(),
+        #'n2o_atm': df_year[ 'n2o_atm' ].sum(),
+        #'surfaceom_c_init': df_year[ 'surfaceom_c' ].values[0],
+        #'surfaceom_c_end': df_year[ 'surfaceom_c' ].values[-1],
+        'leach_no3': df_year[ 'leach_no3' ].sum(),
+        'Rain' : df_year[ 'Rain' ].sum(),
+        'drain' : df_year[ 'drain' ].sum()
+        }
+        if swim == True:
+            swim_data_dict = {
+            'subsurface_drain': df_year[ 'subsurface_drain' ].sum(),
+            'subsurface_drain_no3': df_year[ 'subsurface_drain_no3' ].sum()
+            }
+            data.update(swim_data_dict)
+            push_data.append(data)
+    else:
+        years = daily_df['year'].unique()
+        for i in years:
+            df_year = daily_df.loc[daily_df['year'] == i].reset_index(drop=True)
+            # perform simple analytics at time scale (here we only do year)
+            data = {
+                'title' : df_header,
+                'field' : field,
+                'mukey' : mukey,
+                'rotation' : rotation,
+                'year': int(i),
+                'soybean_yield': df_year[ 'soybean_yield' ].max(),
+                'maize_yield': df_year[ 'maize_yield' ].max(),
+                'soy_mktyd': df_year[ 'soy_mktyd' ].max(),
+                'maz_mktyd': df_year[ 'maz_mktyd' ].max(),
+                'soy_ymgha': df_year[ 'soy_ymgha' ].max(),
+                'maz_ymgha': df_year[ 'maz_ymgha' ].max(),
+                'corn_buac' : df_year[ 'corn_buac' ].max(),
+                'soy_buac' : df_year[ 'soy_buac' ].max(),
+                'soybean_biomass': df_year[ 'soybean_biomass' ].max(),
+                'maize_biomass': df_year[ 'maize_biomass' ].max(),
+                'fertiliser': df_year[ 'fertiliser' ].sum(),
+                #'n2o_atm': df_year[ 'n2o_atm' ].sum(),
+                #'surfaceom_c_init': df_year[ 'surfaceom_c' ].values[0],
+                #'surfaceom_c_end': df_year[ 'surfaceom_c' ].values[-1],
+                'leach_no3': df_year[ 'leach_no3' ].sum(),
+                'Rain': df_year[ 'Rain' ].sum(),
+                'drain' : df_year[ 'drain' ].sum()
+            }
+            if swim == True:
+                swim_data_dict = {
+                'subsurface_drain': df_year[ 'subsurface_drain' ].sum(),
+                'subsurface_drain_no3': df_year[ 'subsurface_drain_no3' ].sum()
+                }
+                data.update(swim_data_dict)
+            
+            push_data.append(data)
+    push_df = pd.DataFrame().append(push_data, ignore_index=True)
+    return push_df
+
+
+def parse_all_output(out_file, year=None): #, db_path, db_schema, db_table
+    """Parses sim output (.out) for a single run, gets daily output and returns df for the given year
+    or several years if year=None.
+    Arguments:
+        out_file {str} -- path to .out file
+        year (int) -- the targeted year of simulation data
+    Returns:
+        [df object] -- dataframe with daily data for each .out file
+    """
+    #dbconn = db.connect_to_db(db_path)
+    push_df = pd.DataFrame()
+    # read file
+    daily_df = pd.read_csv( out_file, header = 3, delim_whitespace = True )
+    daily_df = daily_df.drop( [0] )
+    #get field name, mukey, and rotation with regex
+    df_header = daily_df['title'][1]
+    name_pattern = "name_(.*?)_mukey"
+    field = str(re.search(name_pattern, df_header).group(1))
+    mukey_pattern = "mukey_(.*?)_rot"
+    mukey = str(re.search(mukey_pattern, df_header).group(1))
+    rot_pattern = "rot_(.*?)_sim"
+    rotation = str(re.search(rot_pattern, df_header).group(1))
+    #insert new columns with mukey county fip rot
+    daily_df.insert(1, 'field', field)
+    daily_df.insert(2, 'mukey', mukey)
+    daily_df.insert(3, 'rotation', rotation)
+    daily_df = daily_df.reset_index( drop = True )
+    #replace all ? with NaN
+    daily_df = daily_df.replace("?", np.nan)
+
+    # cast explicit data types for processing
+    daily_df = daily_df.astype( {
+        'date' : 'string',
+        'field' : 'string',
+        'mukey' : 'string',
+        'rotation' : 'string',
+        'day': 'int64',
+        'year': 'int64',
+        'soybean_yield': 'float64',
+        'maize_yield': 'float64',
+        'soy_mktyd': 'float64',
+        'maz_mktyd': 'float64',
+        'soy_ymgha': 'float64',
+        'maz_ymgha': 'float64',
+        'soybean_biomass': 'float64',
+        'maize_biomass': 'float64',
+        'fertiliser': 'float64',
+        #'n2o_atm': 'float64',
+        'surfaceom_c': 'float64',
+        # 'subsurface_drain': 'float64',
+        # 'subsurface_drain_no3': 'float64',
+        'leach_no3': 'float64',
+        'corn_buac' : 'float64',
+        'soy_buac' : 'float64'
+        } )
+    if year:
+        df_year = daily_df.loc[daily_df['year'] == year].reset_index(drop=True)
+        push_df = push_df.append(df_year)
+    else:
+        push_df = push_df.append(daily_df)
+    return push_df
+
+
+
 
 if __name__ == "__main__":
     pass
